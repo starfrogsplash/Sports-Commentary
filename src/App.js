5339add4f2acdb0f0d320c4defac27b62937eee5
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-08-26 13:08:58 +0100

    rendered data on view

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import './App.css';
 class App extends Component {
 
    data = {
-      '12': "Dier goes into the books with his first yellow card",
-      '14': "Kane is desperate to catch liverpool's sala as he runs away from him",
+      '12' : "Dier goes into the books with his first yellow card",
+      '14' : "Kane is desperate to catch liverpool's sala as he runs away from him",
       '18' : "Now Ali gets a booking and the opposing team have a free kick",
       '21' : "First goal is scored by Manchester United, shots fired!"
    }
    
   render() {
+ 
     return (
       <div className="App">
-        <p className="App-intro">
-          Live Commentary
-        </p>
+        <p className="App-intro"> Live Commentary </p>
+        {Object.keys(this.data).map(key => (
+          <div>
+            <div> time: {key} </div> 
+            <div> : {this.data[key]} </div>
+          </div>
+        ))
+        }
       </div>
     );
   }
