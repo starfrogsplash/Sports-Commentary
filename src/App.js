17a26c95e26107cbafe10fe0b3171f5148f3487b
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-08-26 12:47:58 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+   data = {
+      '12': "Dier goes into the books with his first yellow card",
+      '14': "Kane is desperate to catch liverpool's sala as he runs away from him",
+      '18' : "Now Ali gets a booking and the opposing team have a free kick",
+      '21' : "First goal is scored by Manchester United, shots fired!"
+   }
+   
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          Live Commentary
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
