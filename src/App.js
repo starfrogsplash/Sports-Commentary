ceb2be3dac9c0f55a75de337a9b43d344a6389c7
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-08-26 16:57:03 +0100

    styled App

--- src/App.js
+++ src/App.js
@@ -5,55 +5,96 @@ class App extends Component {
    data = {
       '12' : "Dier goes into the books with his first yellow card",
       '14' : "Kane is desperate to catch liverpool's sala as he runs away from him",
-      '18' : "Now Ali gets a booking and the opposing team have a free kick",
-      '21' : "First goal is scored by Manchester United, shots fired!"
+      '18' : `Now John gets a booking and the opposing team have a free kick
+            but Mike tackled him aggressively
+            Now John takes the penalty`,
+      '21' : "First goal is scored by Manchester United, shots fired!",
+      '27' : "2nd goal is scored by Liverpool Fc, shots fired!",
+      '30' : "Dier goes into the books with his first yellow card",
    }
    
   render() {
- 
     return (
       <div >
-        <p className="App-intro"> Live Commentary </p>
-        <div className="structure">
-              {Object.keys(this.data).map(key => (
-                  <div key={key} className="content">
-                    <div className="circle"> {key} </div> 
-                    <div className="text"> {this.data[key]} </div>
-                  </div>
-                ))
-              }
+          <h2 className= "title"> Live Commentary </h2>
+            <div className="structure">
             <div className="line"/>
-        </div>
+                  {Object.keys(this.data).map(key => (
+                      <div key={key} className="content">
+                            <div className="circle-container">
+                              <div className="circle"> {key} </div> 
+                            </div>
+                          <div className="text"> {this.data[key]} </div>
+                      </div>
+                    ))
+                  }
+            </div>
         <style jsx>{`
+  
+        .title{
+          margin: 50px 10px 10px 10px;
+          color: rgb(56, 71, 128);
+          text-decoration: underline;
+          font-size: 40px;
+        }
+
         .structure {
             display: flex;
             flex-direction: column;
+            height: 100vh;
+            margin: 10px;
+            top: 200px;
+          }
+
+         .structure ,.line{
+          position: absolute;
+         } 
+
+          .content {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
           }
 
          .line {
-          background-color: blue;
-          height: 300px;
-          width: 30px;
+          border-right: 5px solid blue;
+          box-sizing: border-box;
+          height: 100%;
+          width: 24px;
+          top:6px;
+        } 
+
+        .text{
+          margin-left: 10px;
+          border-radius: 10px;
+          box-shadow: 5px  5px  10px rgba(0,0,0,0.5);
+          padding: 10px;
+          background-color:  rgb(220, 223, 235);
+          color: rgb(56, 71, 128);
         }
 
-        .content {
+        .circle-container{
           display: flex;
-          justify-content: space-between;
+          align-items: center;
+          justify-content: center;
         }
-        
+       
         .circle {
+            margin: 0 10px 10px 0;
+            z-index: 1;
             display: flex;
             align-items: center;
             justify-content: center;
-            height: 50px;
-            width: 50px;
+            box-shadow: 3px  3px  10px rgba(0,0,0,0.5);
+            height: 40px;
+            width: 40px;
             border-radius: 50px;
-            border: 3px solid;
+            color: rgb(56, 71, 128);
+            background-color: rgb(160, 175, 233);
           }
-        
+
         `
         }
-
         </style>
       </div>
     );
